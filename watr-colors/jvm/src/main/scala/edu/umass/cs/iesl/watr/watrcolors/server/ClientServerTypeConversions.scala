de87480b5ae5010159d09182f4ca75979fb55f00
Adam Saunders <saunders@cs.umass.edu>
2016-12-21 18:27:27 -0500

    refactoring projects for better js/jvm data sharing

--- watr-colors/jvm/src/main/scala/edu/umass/cs/iesl/watr/watrcolors/server/ClientServerTypeConversions.scala
+++ watr-colors/jvm/src/main/scala/edu/umass/cs/iesl/watr/watrcolors/server/ClientServerTypeConversions.scala
@@ -2,32 +2,32 @@ package edu.umass.cs.iesl.watr
 package watrcolors
 package server
 
-import textboxing.{TextBoxing => TB}
+// import textboxing.{TextBoxing => TB}
 
-object TypeConverters {
+// object TypeConverters {
 
-  import SharedTypeConversions._
+//   import SharedTypeConversions._
 
-  def convertComponent(cc1: spindex.Component): watrcolors.Component = {
-    ???
-  }
+//   def convertComponent(cc1: spindex.Component): watrcolors.Component = {
+//     ???
+//   }
 
-  def convertVisualTraceTypes(cc1: utils.TraceLog): TraceLog = {
-    cc1 match {
-      case utils.TraceLog.Noop                                => TraceLog.Noop
-      case utils.TraceLog.SetPageGeometries(pgs)              => TraceLog.SetPageGeometries(pgs.map(_.convert()))
-      case utils.TraceLog.Show(s: Seq[spindex.TargetRegion])  => TraceLog.Show(s.map(_.convert))
-      case utils.TraceLog.ShowZone(s: spindex.Zone)           => TraceLog.ShowZone(s.convert)
-      case utils.TraceLog.ShowComponent(s: spindex.Component) => TraceLog.ShowComponent(s.convert)
-      case utils.TraceLog.ShowLabel(l: watrmarks.Label)       => TraceLog.ShowLabel(l.convert)
-      case utils.TraceLog.FocusOn(s: spindex.TargetRegion)    => TraceLog.FocusOn(s.convert)
-      case utils.TraceLog.Indicate(s: spindex.TargetFigure)   => TraceLog.Indicate(s.convert)
-      case utils.TraceLog.Message(s: TB.Box)                  => TraceLog.Message(s.toString)
-      case utils.TraceLog.All(ts)                             => TraceLog.All(ts.map(convertVisualTraceTypes(_)))
-      case utils.TraceLog.Link(ts)                            => TraceLog.Link(ts.map(convertVisualTraceTypes(_)))
-      case utils.TraceLog.Group(name, ts)                     => TraceLog.Group(name, ts.map(convertVisualTraceTypes(_)))
-      case utils.TraceLog.GroupEnd(name)                      => TraceLog.GroupEnd(name)
+//   def convertVisualTraceTypes(cc1: utils.TraceLog): TraceLog = {
+//     cc1 match {
+//       case tracing.TraceLog.Noop                                => TraceLog.Noop
+//       case tracing.TraceLog.SetPageGeometries(pgs)              => TraceLog.SetPageGeometries(pgs.map(_.convert()))
+//       case tracing.TraceLog.Show(s: Seq[spindex.TargetRegion])  => TraceLog.Show(s.map(_.convert))
+//       case tracing.TraceLog.ShowZone(s: spindex.Zone)           => TraceLog.ShowZone(s.convert)
+//       case tracing.TraceLog.ShowComponent(s: spindex.Component) => TraceLog.ShowComponent(s.convert)
+//       case tracing.TraceLog.ShowLabel(l: watrmarks.Label)       => TraceLog.ShowLabel(l.convert)
+//       case tracing.TraceLog.FocusOn(s: spindex.TargetRegion)    => TraceLog.FocusOn(s.convert)
+//       case tracing.TraceLog.Indicate(s: spindex.TargetFigure)   => TraceLog.Indicate(s.convert)
+//       case tracing.TraceLog.Message(s: TB.Box)                  => TraceLog.Message(s.toString)
+//       case tracing.TraceLog.All(ts)                             => TraceLog.All(ts.map(convertVisualTraceTypes(_)))
+//       case tracing.TraceLog.Link(ts)                            => TraceLog.Link(ts.map(convertVisualTraceTypes(_)))
+//       case tracing.TraceLog.Group(name, ts)                     => TraceLog.Group(name, ts.map(convertVisualTraceTypes(_)))
+//       case tracing.TraceLog.GroupEnd(name)                      => TraceLog.GroupEnd(name)
 
-    }
-  }
-}
+//     }
+//   }
+// }
--- watr-colors/jvm/src/main/scala/edu/umass/cs/iesl/watr/watrcolors/server/SharedTypeConversions.scala
+++ watr-colors/jvm/src/main/scala/edu/umass/cs/iesl/watr/watrcolors/server/SharedTypeConversions.scala
@@ -3,88 +3,88 @@ package watrcolors
 package server
 
 
-object SharedTypeConversions {
-  import geometry._
+// object SharedTypeConversions {
+//   import geometry._
 
-  import GeometricFigure._
-  import TypeTags._
-
-
-  implicit class RicherPoint(val in: spindex.GeometricFigure.Point) extends AnyVal {
-    def convert() = Point(in.x, in.y)
-  }
-  implicit class RicherLTBounds(val in: spindex.GeometricFigure.LTBounds) extends AnyVal {
-    def convert() = LTBounds(in.left, in.top, in.width, in.height)
-  }
-  implicit class RicherLBBounds(val in: spindex.GeometricFigure.LBBounds) extends AnyVal {
-    def convert() = LBBounds(in.left, in.bottom, in.width, in.height)
-  }
-
-  implicit class RicherLine(val in: spindex.GeometricFigure.Line) extends AnyVal {
-    def convert(): Line = Line(in.p1.convert(), in.p2.convert())
-  }
-
-  def convertFigure(in: spindex.GeometricFigure): GeometricFigure = convert(in)
-
-  def convert(in: spindex.GeometricFigure): GeometricFigure = {
-    in match {
-      case v: spindex.GeometricFigure.LTBounds => v.convert()
-      case v: spindex.GeometricFigure.LBBounds => v.convert()
-      case v: spindex.GeometricFigure.Point    => v.convert()
-      case v: spindex.GeometricFigure.Line     => v.convert()
-    }
-  }
+//   import GeometricFigure._
+//   import TypeTags._
 
 
-  implicit class RicherPageGeometry(val in: spindex.PageGeometry) extends AnyVal {
-    def convert(): PageGeometry = PageGeometry(
-      in.id.unwrap,
-      in.bounds.convert
-    )
-  }
-  implicit class RicherTargetFigure(val in: spindex.TargetFigure) extends AnyVal {
-    def convert(): TargetFigure = TargetFigure(
-      in.id.unwrap,
-      in.page.unwrap,
-      convertFigure(in.figure)
-    )
-  }
+//   implicit class RicherPoint(val in: spindex.GeometricFigure.Point) extends AnyVal {
+//     def convert() = Point(in.x, in.y)
+//   }
+//   implicit class RicherLTBounds(val in: spindex.GeometricFigure.LTBounds) extends AnyVal {
+//     def convert() = LTBounds(in.left, in.top, in.width, in.height)
+//   }
+//   implicit class RicherLBBounds(val in: spindex.GeometricFigure.LBBounds) extends AnyVal {
+//     def convert() = LBBounds(in.left, in.bottom, in.width, in.height)
+//   }
 
-  implicit class RicherTargetRegion(val in: spindex.TargetRegion) extends AnyVal {
-    def convert(): TargetRegion = TargetRegion(
-      in.id.unwrap,
-      in.target.unwrap,
-      in.bbox.convert()
-    )
-  }
-  implicit class RicherZone(val in: spindex.Zone) extends AnyVal {
-    def convert(): Zone = Zone(
-      in.id.unwrap,
-      in.regions.map(_.convert())
-    )
-  }
+//   implicit class RicherLine(val in: spindex.GeometricFigure.Line) extends AnyVal {
+//     def convert(): Line = Line(in.p1.convert(), in.p2.convert())
+//   }
 
-  import watrmarks.{StandardLabels => LB}
+//   def convertFigure(in: spindex.GeometricFigure): GeometricFigure = convert(in)
 
-  implicit class RicherComponent(val in: spindex.Component) extends AnyVal {
-
-    def convert(): Component = {
-      // val content: Option[String] = if (in.getLabels.contains(LB.TokenizedLine)) {
-      //   Some(in.toText)
-      // } else {
-      //   None
-      // }
-
-      Component(
-        in.id.unwrap,
-        in.targetRegion.convert,
-        None // content
-      )
-    }
-  }
+//   def convert(in: spindex.GeometricFigure): GeometricFigure = {
+//     in match {
+//       case v: spindex.GeometricFigure.LTBounds => v.convert()
+//       case v: spindex.GeometricFigure.LBBounds => v.convert()
+//       case v: spindex.GeometricFigure.Point    => v.convert()
+//       case v: spindex.GeometricFigure.Line     => v.convert()
+//     }
+//   }
 
 
-  implicit class RicherLabel(val in: watrmarks.Label) extends AnyVal {
-    def convert(): watrcolors.Label = watrcolors.Label(in.ns, in.key, in.value)
-  }
-}
+//   implicit class RicherPageGeometry(val in: spindex.PageGeometry) extends AnyVal {
+//     def convert(): PageGeometry = PageGeometry(
+//       in.id.unwrap,
+//       in.bounds.convert
+//     )
+//   }
+//   implicit class RicherTargetFigure(val in: spindex.TargetFigure) extends AnyVal {
+//     def convert(): TargetFigure = TargetFigure(
+//       in.id.unwrap,
+//       in.page.unwrap,
+//       convertFigure(in.figure)
+//     )
+//   }
+
+//   implicit class RicherTargetRegion(val in: spindex.TargetRegion) extends AnyVal {
+//     def convert(): TargetRegion = TargetRegion(
+//       in.id.unwrap,
+//       in.target.unwrap,
+//       in.bbox.convert()
+//     )
+//   }
+//   implicit class RicherZone(val in: spindex.Zone) extends AnyVal {
+//     def convert(): Zone = Zone(
+//       in.id.unwrap,
+//       in.regions.map(_.convert())
+//     )
+//   }
+
+//   import watrmarks.{StandardLabels => LB}
+
+//   implicit class RicherComponent(val in: spindex.Component) extends AnyVal {
+
+//     def convert(): Component = {
+//       // val content: Option[String] = if (in.getLabels.contains(LB.TokenizedLine)) {
+//       //   Some(in.toText)
+//       // } else {
+//       //   None
+//       // }
+
+//       Component(
+//         in.id.unwrap,
+//         in.targetRegion.convert,
+//         None // content
+//       )
+//     }
+//   }
+
+
+//   implicit class RicherLabel(val in: watrmarks.Label) extends AnyVal {
+//     def convert(): watrcolors.Label = watrcolors.Label(in.ns, in.key, in.value)
+//   }
+// }
