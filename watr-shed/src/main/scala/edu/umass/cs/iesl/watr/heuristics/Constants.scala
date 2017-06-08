281f29bc25a3266729f752cba190e5d0dd1c05a2
adityanshastry <adityashastry1992@gmail.com>
2017-06-08 09:55:23 -0400

    Global Constants to be used for heuristics

---
+++ watr-shed/src/main/scala/edu/umass/cs/iesl/watr/heuristics/Constants.scala
@@ -0,0 +1,13 @@
+package edu.umass.cs.iesl.watr.heuristics
+
+object Constants {
+
+    final val WORD_SEPARATORS: Array[String] = Array("and", "&")
+    final val PUNCTUATION_SEPARATORS: Array[String] = Array(",")
+    final val NAME_SEPARATOR: String = " "
+    final val SPACE_BETWEEN_WORDS_THRESHOLD: Int = 200
+    final val COMMA: String = ","
+    final val DOT: Char = '.'
+    final val BLANK: String = ""
+
+}
