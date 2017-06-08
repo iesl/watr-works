b97bc0d5a3c3c65a4428961f983951e8a22b1e3f
adityanshastry <adityashastry1992@gmail.com>
2017-06-08 09:56:23 -0400

    Reduce runtime using while and indices

--- watr-shed/src/main/scala/edu/umass/cs/iesl/watr/heuristics/AuthorNameHeuristics.scala
+++ watr-shed/src/main/scala/edu/umass/cs/iesl/watr/heuristics/AuthorNameHeuristics.scala
@@ -2,38 +2,39 @@ package edu.umass.cs.iesl.watr.heuristics
 
 import edu.umass.cs.iesl.watr.textreflow.data.TextReflow
 import edu.umass.cs.iesl.watr.textreflow.data._
+
 import util.control.Breaks._
+import Constants._
+import edu.umass.cs.iesl.watr.geometry.CharAtom
 
 import scala.collection.mutable.ListBuffer
 
 object AuthorNameHeuristics {
 
-    def tokenizeAuthorNames(authorTextReflow: TextReflow): ListBuffer[String] ={
-        val authorNamesTokens : ListBuffer[String] = ListBuffer[String]()
-        val authorName : ListBuffer[Char] = ListBuffer[Char]()
-
-        val spaceThreshold: Int = 200
+    def tokenizeAuthorNames(authorTextReflow: TextReflow): ListBuffer[String] = {
+        val authorNamesTokens: ListBuffer[String] = ListBuffer[String]()
+        val authorName: ListBuffer[Char] = ListBuffer[Char]()
 
         var prevCharPosition: Int = -1
         var yPosition: Int = -1
 
-        for (charAtom <- authorTextReflow.charAtoms()){
+        for (charAtom <- authorTextReflow.charAtoms()) {
             val currentCharacter = charAtom.char.toCharArray.head
-            if(currentCharacter.isLetter && yPosition.==(-1)){
+            if (currentCharacter.isLetter && yPosition.==(-1)) {
                 yPosition = charAtom.bbox.top.asInstanceOf[Int]
-                if(prevCharPosition.==(-1)){
+                if (prevCharPosition.==(-1)) {
                     authorName += currentCharacter
                 }
             }
-            if(prevCharPosition > 0){
+            if (prevCharPosition > 0) {
                 val spaceBetweenChars = charAtom.bbox.left.asInstanceOf[Int] - prevCharPosition
-                if(spaceBetweenChars < spaceThreshold && charAtom.bbox.top.==(yPosition)){
+                if (spaceBetweenChars < SPACE_BETWEEN_WORDS_THRESHOLD && charAtom.bbox.top.==(yPosition)) {
                     authorName += currentCharacter
                 }
-                else if(spaceBetweenChars >= spaceThreshold){
+                else if (spaceBetweenChars >= SPACE_BETWEEN_WORDS_THRESHOLD) {
                     authorNamesTokens += authorName.mkString
                     authorName.clear()
-                    if(charAtom.bbox.top.==(yPosition)){
+                    if (charAtom.bbox.top.==(yPosition)) {
                         authorName += currentCharacter
                     }
                 }
@@ -41,7 +42,7 @@ object AuthorNameHeuristics {
             prevCharPosition = charAtom.bbox.left.asInstanceOf[Int] + charAtom.bbox.width.asInstanceOf[Int]
         }
 
-        if(authorName.nonEmpty){
+        if (authorName.nonEmpty) {
             authorNamesTokens += authorName.mkString
             authorName.clear()
         }
@@ -49,49 +50,57 @@ object AuthorNameHeuristics {
         authorNamesTokens
     }
 
-    def isOfFirstNameInitialFormat(authorNameComponent: String): Boolean ={
+    def isOfFirstNameInitialFormat(authorNameComponent: String): Boolean = {
 
-        if(authorNameComponent.head.isUpper && authorNameComponent.charAt(1).==('.')){
+        if (authorNameComponent.head.isUpper && authorNameComponent.charAt(1).==(DOT)) {
             return true
         }
         false
     }
 
-    def getSeparateAuthorNamesByText(authorNamesTokens: ListBuffer[String]): ListBuffer[String] ={
+    def getNextNameAndComponentIndices(currentAuthorNameComponentIndex: Int, currentAuthorNameIndex: Int, currentAuthorName: String): (Int, Int) = {
 
-        val separateAuthorNames : ListBuffer[String] = ListBuffer[String]()
-        val separateAuthorName : ListBuffer[String] = ListBuffer[String]()
+        if (currentAuthorNameComponentIndex + 1 == currentAuthorName.split(NAME_SEPARATOR).length) {
+            return (0, currentAuthorNameIndex + 1)
+        }
+        (currentAuthorNameComponentIndex + 1, currentAuthorNameIndex)
+    }
+
+    def getSeparateAuthorNamesByText(authorNamesTokens: ListBuffer[String]): ListBuffer[String] = {
+
+        val separateAuthorNames: ListBuffer[String] = ListBuffer[String]()
+        val separateAuthorName: ListBuffer[String] = ListBuffer[String]()
 
         var checkNextFlag: Boolean = false
 
-        for(authorNameToken <- authorNamesTokens){
-            breakable{
-                if(authorNameToken.equalsIgnoreCase("and") || authorNameToken.equalsIgnoreCase("&")){
-                    separateAuthorNames += separateAuthorName.mkString(" ")
+        for (authorNameToken <- authorNamesTokens) {
+            breakable {
+                if (WORD_SEPARATORS.contains(authorNameToken.toLowerCase)) {
+                    separateAuthorNames += separateAuthorName.mkString(NAME_SEPARATOR)
                     separateAuthorName.clear()
-                    if(checkNextFlag){
+                    if (checkNextFlag) {
                         checkNextFlag = false
                     }
                 }
-                else if(!checkNextFlag && authorNameToken.takeRight(n = 1).equals(",")){
-                    separateAuthorName += authorNameToken.replace(",", "")
+                else if (!checkNextFlag && PUNCTUATION_SEPARATORS.contains(authorNameToken.takeRight(n = 1))) {
+                    separateAuthorName += authorNameToken.replace(authorNameToken.takeRight(n = 1), BLANK)
                     checkNextFlag = true
                     break
                 }
-                else if(checkNextFlag){
-                    if(isOfFirstNameInitialFormat(authorNameToken)){
-                        separateAuthorName += authorNameToken.replace(",", "")
-                        separateAuthorNames += separateAuthorName.mkString(" ")
+                else if (checkNextFlag) {
+                    if (isOfFirstNameInitialFormat(authorNameToken)) {
+                        separateAuthorName += authorNameToken.replace(COMMA, BLANK)
+                        separateAuthorNames += separateAuthorName.mkString(NAME_SEPARATOR)
                         separateAuthorName.clear()
                     }
                     else {
-                        separateAuthorNames += separateAuthorName.mkString(" ")
+                        separateAuthorNames += separateAuthorName.mkString(NAME_SEPARATOR)
                         separateAuthorName.clear()
                         separateAuthorName += authorNameToken
                     }
                     checkNextFlag = false
                 }
-                else{
+                else {
                     separateAuthorName += authorNameToken
                 }
 
@@ -99,101 +108,65 @@ object AuthorNameHeuristics {
 
         }
 
-        if(separateAuthorName.nonEmpty){
-            separateAuthorNames += separateAuthorName.mkString(" ")
+        if (separateAuthorName.nonEmpty) {
+            separateAuthorNames += separateAuthorName.mkString(NAME_SEPARATOR)
         }
         separateAuthorNames
     }
 
-    def getSeparateAuthorNamesByGeometry(authorNamesSeparatedByText: ListBuffer[String], authorTextReflow: TextReflow): ListBuffer[String] ={
+    def getSeparateAuthorNamesByGeometry(authorNamesSeparatedByText: ListBuffer[String], authorTextReflow: TextReflow): ListBuffer[String] = {
 
-        val separateAuthorNames : ListBuffer[String] = ListBuffer[String]()
-        val separateAuthorName : ListBuffer[String] = ListBuffer[String]()
+        val separateAuthorNames: ListBuffer[String] = ListBuffer[String]()
+        val separateAuthorName: ListBuffer[String] = ListBuffer[String]()
 
         var currentAuthorNameIndex: Int = 0
         var currentAuthorName: String = authorNamesSeparatedByText(currentAuthorNameIndex)
         var currentAuthorNameComponentIndex: Int = 0
-        var currentAuthorNameComponent: String = currentAuthorName.split(" ")(currentAuthorNameComponentIndex)
+        var currentAuthorNameComponent: String = currentAuthorName.split(NAME_SEPARATOR)(currentAuthorNameComponentIndex)
         var prevCharPosition: Int = -1
         var usualSpaceWidth: Int = 0
         var processingComplete: Boolean = false
+        var currentCharAtomIndex: Int = 0
 
-        breakable {
-            for (charAtom <- authorTextReflow.charAtoms()) {
-                breakable {
-                    val currentCharacter = charAtom.char.toCharArray.head
-                    if (!currentCharacter.equals(currentAuthorNameComponent.head)) {
-                        break
+        while (currentCharAtomIndex < authorTextReflow.charAtoms().length && currentAuthorNameIndex < authorNamesSeparatedByText.length) {
+            var charAtom: CharAtom = authorTextReflow.charAtoms()(currentCharAtomIndex)
+            currentAuthorNameComponent = currentAuthorName.split(NAME_SEPARATOR)(currentAuthorNameComponentIndex)
+            currentAuthorName = authorNamesSeparatedByText(currentAuthorNameIndex)
+            val currentCharacter = charAtom.char.toCharArray.head
+            if (currentCharacter.equals(currentAuthorNameComponent.head)) {
+                if (prevCharPosition < 0) {
+                    separateAuthorName += currentAuthorNameComponent
+                }
+                else {
+                    if (usualSpaceWidth == 0) {
+                        usualSpaceWidth = charAtom.bbox.left.asInstanceOf[Int] - prevCharPosition
                     }
                     else {
-                        if (prevCharPosition < 0) {
-                            separateAuthorName += currentAuthorNameComponent
-                            currentAuthorNameComponentIndex += 1
-                            if (currentAuthorNameComponentIndex == currentAuthorName.split(" ").length) {
-                                currentAuthorNameIndex += 1
-                                if (currentAuthorNameIndex == authorNamesSeparatedByText.length) {
-                                    processingComplete = true
-                                    break
-                                }
-                                else {
-                                    currentAuthorName = authorNamesSeparatedByText(currentAuthorNameIndex)
-                                }
-                                currentAuthorNameComponentIndex = 0
-                            }
-                            currentAuthorNameComponent = currentAuthorName.split(" ")(currentAuthorNameComponentIndex)
-                            break
-                        }
-                        else {
-                            if (usualSpaceWidth == 0) {
-                                usualSpaceWidth = charAtom.bbox.left.asInstanceOf[Int] - prevCharPosition
-                            }
-                            else {
-                                if ((charAtom.bbox.left.asInstanceOf[Int] - prevCharPosition) > 2 * usualSpaceWidth) {
-                                    separateAuthorNames += separateAuthorName.mkString(" ")
-                                    separateAuthorName.clear()
-                                    separateAuthorName += currentAuthorNameComponent
-                                    currentAuthorNameComponentIndex += 1
-                                    if (currentAuthorNameComponentIndex == currentAuthorName.split(" ").length) {
-                                        currentAuthorNameIndex += 1
-                                        if (currentAuthorNameIndex == authorNamesSeparatedByText.length) {
-                                            processingComplete = true
-                                            break
-                                        }
-                                        else {
-                                            currentAuthorName = authorNamesSeparatedByText(currentAuthorNameIndex)
-                                        }
-                                        currentAuthorNameComponentIndex = 0
-                                    }
-                                    currentAuthorNameComponent = currentAuthorName.split(" ")(currentAuthorNameComponentIndex)
-                                }
-                            }
-                            separateAuthorName += currentAuthorNameComponent
-                            currentAuthorNameComponentIndex += 1
-                            if (currentAuthorNameComponentIndex == currentAuthorName.split(" ").length) {
-                                separateAuthorNames += separateAuthorName.mkString(" ")
-                                separateAuthorName.clear()
-                                currentAuthorNameIndex += 1
-                                if (currentAuthorNameIndex == authorNamesSeparatedByText.length) {
-                                    processingComplete = true
-                                    break
-                                }
-                                else {
-                                    currentAuthorName = authorNamesSeparatedByText(currentAuthorNameIndex)
-                                }
-                                currentAuthorNameComponentIndex = 0
-                            }
-                            currentAuthorNameComponent = currentAuthorName.split(" ")(currentAuthorNameComponentIndex)
+                        if ((charAtom.bbox.left.asInstanceOf[Int] - prevCharPosition) > 2 * usualSpaceWidth) {
+                            separateAuthorNames += separateAuthorName.mkString(NAME_SEPARATOR)
+                            separateAuthorName.clear()
                         }
                     }
+                    separateAuthorName += currentAuthorNameComponent
+                    if (currentAuthorNameComponentIndex + 1 == currentAuthorName.split(NAME_SEPARATOR).length) {
+                        separateAuthorNames += separateAuthorName.mkString(NAME_SEPARATOR)
+                        separateAuthorName.clear()
+                    }
                 }
-                prevCharPosition = charAtom.bbox.left.asInstanceOf[Int] + charAtom.bbox.width.asInstanceOf[Int]
-                if (processingComplete) {
-                    break
-                }
+
+                currentCharAtomIndex += currentAuthorNameComponent.length - 1
+                val indices = getNextNameAndComponentIndices(currentAuthorNameComponentIndex, currentAuthorNameIndex, currentAuthorName)
+                currentAuthorNameComponentIndex = indices._1
+                currentAuthorNameIndex = indices._2
+
             }
+            charAtom = authorTextReflow.charAtoms()(currentCharAtomIndex)
+            prevCharPosition = charAtom.bbox.left.asInstanceOf[Int] + charAtom.bbox.width.asInstanceOf[Int]
+            currentCharAtomIndex += 1
         }
 
-        if(separateAuthorNames.nonEmpty){
+
+        if (separateAuthorNames.nonEmpty) {
             return separateAuthorNames
         }
         authorNamesSeparatedByText
