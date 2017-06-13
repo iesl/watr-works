21318290a97909a9c95a489a322bd69ed6dd62f6
adityanshastry <adityashastry1992@gmail.com>
2017-06-13 11:46:22 -0400

    Name suffixes, and last name participles

--- watr-shed/src/main/scala/edu/umass/cs/iesl/watr/heuristics/Constants.scala
+++ watr-shed/src/main/scala/edu/umass/cs/iesl/watr/heuristics/Constants.scala
@@ -2,8 +2,8 @@ package edu.umass.cs.iesl.watr.heuristics
 
 object Constants {
 
-    final val WORD_SEPARATORS: Array[String] = Array("and", "&")
-    final val PUNCTUATION_SEPARATORS: Array[String] = Array(",")
+    final val WORD_SEPARATORS: Seq[String] = Seq("and", "&")
+    final val PUNCTUATION_SEPARATORS: Seq[String] = Seq(",")
     final val NAME_SEPARATOR: String = " "
     final val SPACE_BETWEEN_WORDS_THRESHOLD: Int = 200
     final val COMMA: String = ","
@@ -11,5 +11,10 @@ object Constants {
     final val DOT: Char = '.'
     final val PERIOD: String =  "."
     final val BLANK: String = ""
+    final val VALID_HEREDITY_SUFFIXES: Seq[String] = Seq("Jr.", "Sr.", "II", "III", "IV")
+    final val VALID_SURNAME_PARTICLES: Seq[String] = Seq("van", "von", "der", "de", "du", "da", "di", "do", "la", "del", "della", "ter", "bin", "ben")
+    final val FIRST_NAME: String = "first_name"
+    final val MIDDLE_NAME: String = "middle_name"
+    final val LAST_NAME: String = "last_name"
 
 }
