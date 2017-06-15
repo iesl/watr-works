f858b2dda23662291f71fe399b97d6d83d710ebd
adityanshastry <adityashastry1992@gmail.com>
2017-06-15 17:49:26 -0400

    refactored name/ update surname participle

--- watr-shed/src/main/scala/edu/umass/cs/iesl/watr/heuristics/Constants.scala
+++ watr-shed/src/main/scala/edu/umass/cs/iesl/watr/heuristics/Constants.scala
@@ -4,7 +4,7 @@ object Constants {
 
     final val WORD_SEPARATORS: Seq[String] = Seq("and", "&")
     final val PUNCTUATION_SEPARATORS: Seq[String] = Seq(",")
-    final val NAME_SEPARATOR: String = " "
+    final val SPACE_SEPARATOR: String = " "
     final val SPACE_BETWEEN_WORDS_THRESHOLD: Int = 200
     final val COMMA: String = ","
     final val COMMA_CHARACTER: Char = ','
@@ -12,7 +12,7 @@ object Constants {
     final val PERIOD: String =  "."
     final val BLANK: String = ""
     final val VALID_HEREDITY_SUFFIXES: Seq[String] = Seq("Jr.", "Sr.", "II", "III", "IV")
-    final val VALID_SURNAME_PARTICLES: Seq[String] = Seq("van", "von", "der", "de", "du", "da", "di", "do", "la", "del", "della", "ter", "bin", "ben")
+    final val VALID_SURNAME_PARTICLES: Seq[String] = Seq("van", "von", "der", "de", "du", "da", "di", "do", "la", "del", "della", "ter", "bin", "ben", "den")
     final val FIRST_NAME: String = "first_name"
     final val MIDDLE_NAME: String = "middle_name"
     final val LAST_NAME: String = "last_name"
