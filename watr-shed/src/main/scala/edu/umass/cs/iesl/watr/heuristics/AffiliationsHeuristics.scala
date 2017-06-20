fb89c2b566f55a2feac1f251416abf478ce45be4
adityanshastry <adityashastry1992@gmail.com>
2017-06-20 14:18:30 -0400

    specific heuristics for affliations

---
+++ watr-shed/src/main/scala/edu/umass/cs/iesl/watr/heuristics/AffiliationsHeuristics.scala
@@ -0,0 +1,48 @@
+package edu.umass.cs.iesl.watr
+package heuristics
+
+import Constants._
+
+import scala.collection.mutable.ListBuffer
+
+object AffiliationsHeuristics {
+
+    def getSeparateAffiliationComponentsByText(tokenizedTextReflow: ListBuffer[String]): ListBuffer[String] = {
+
+        val separateComponents: ListBuffer[String] = ListBuffer[String]()
+        val separateComponent: ListBuffer[String] = ListBuffer[String]()
+
+        for (textReflowToken <- tokenizedTextReflow) {
+            if( PUNCTUATION_SEPARATORS.contains(textReflowToken.takeRight(n = 1))){
+                separateComponent += textReflowToken.dropRight(n = 1)
+                separateComponents += separateComponent.mkString(SPACE_SEPARATOR)
+                separateComponent.clear()
+            }
+            else {
+                separateComponent += textReflowToken
+            }
+        }
+
+        if (separateComponent.nonEmpty) {
+            separateComponents += separateComponent.mkString(SPACE_SEPARATOR)
+        }
+
+        for(separateComponent <- separateComponents if separateComponent.length.==(0)){
+            separateComponents -= separateComponent
+        }
+
+        var currentComponentIndex: Int = 0
+
+        while(currentComponentIndex < separateComponents.length){
+            val currentComponent = separateComponents(currentComponentIndex)
+            for(conjunction <- WORD_SEPARATORS if currentComponent.split(SPACE_SEPARATOR).length.==(3) && currentComponent.split(conjunction).length.==(2)){
+                separateComponents(currentComponentIndex - 1) = separateComponents.slice(currentComponentIndex-1, currentComponentIndex+1).mkString(COMMA.concat(SPACE_SEPARATOR))
+                separateComponents -= currentComponent
+            }
+            currentComponentIndex += 1
+        }
+        separateComponents
+    }
+
+
+}
