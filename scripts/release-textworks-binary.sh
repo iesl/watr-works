25495959dc0d4cb18d2a6b7218931ebef4b1b24d
Adam Saunders <saunders@cs.umass.edu>
2019-07-08 13:15:59 -0400

    pre-release

--- scripts/release-textworks-binary.sh
+++ scripts/release-textworks-binary.sh
@@ -1,63 +1,124 @@
 #!/bin/bash
 
-# creates a GitHub release (draft) and adds pre-built artifacts to the release
-# after running this script user should manually check the release in GitHub, optionally edit it, and publish it
-
-# args: :version_number (the version number of this release), :body (text describing the contents of the tag)
-# example usage: ./gh_release_bamboo.sh "1.0.0" "Release notes: ..."
-# => name: nRF5-ble-driver_<platform_name>_1.0.0_compiled-binaries.zip example: nRF5-ble-driver_win-64_2.0.1_compiled-binaries.zip
-
-# to ensure that bash is used: https://answers.atlassian.com/questions/28625/making-a-bamboo-script-execute-using-binbash
-if [ "$(ps -p "$$" -o comm=)" != "bash" ]; then
-    bash "$0" "$@"
-    exit "$?"
-fi
-
 # define variables
-GH_ACCESS_TOKEN="9d103742269e07343b28b56c9f939e3303f3a05c"
+# GH_ACCESS_TOKEN="9d103742269e07343b28b56c9f939e3303f3a00"
+GH_ACCESS_TOKEN="5f10c573fec1632bcb5cdb9276613ce91afb4a26"
 GH_API="https://api.github.com"
+GH_UPLOAD="https://uploads.github.com"
 GH_OWNER="iesl"
-GH_REPO="$GH_API/repos/$GH_OWNER/watr-works/releases?access_token=$GH_ACCESS_TOKEN"
 
-# create a GitHub release, see: https://www.barrykooij.com/create-github-releases-via-command-line/
-RELEASE_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "nRF5-ble-driver_%s","body": "%s","draft": true,"prerelease": false}' "$1" "$1" "$2")
-echo "Creating a release with info: $RELEASE_JSON."
+release_id="v0.11"
 
-# release_id="$(curl --data "$RELEASE_JSON" $GH_REPO | python -c "import sys, json; print json.load(sys.stdin)['id']")"
+# GH_REPO="$GH_API/repos/$GH_OWNER/watr-works/branches/branch/acs-dev/releases/latest?access_token=$GH_ACCESS_TOKEN"
+# GH_REPO="$GH_API/repos/$GH_OWNER/watr-works/branches/acs-dev?access_token=$GH_ACCESS_TOKEN"
+GH_REPO="$GH_API/repos/$GH_OWNER/watr-works/releases/tags/$release_id?access_token=$GH_ACCESS_TOKEN"
 
-# # exit script if GitHub release was not successfully created
-# if [ -z "$release_id" ]
-# then
-#       echo "Failed to create GitHub release. Exiting with error."
-#       exit 1
-# else
-#       echo "Release created with id: $release_id."
-# fi
+POST "$GH_REPO"
 
-# # add artifacts to the release
-# # note that we expect artifacts for each platform to be located in separate sub-dirs,
-# # and we add these zipped sub-dirs to the release
-# ARTIFACTS_DIR="./artifacts"
-# for path in "$ARTIFACTS_DIR"/*; do
-#     # if not a directory, skip
-#     [ -d "$path" ] || continue
 
-#     dirname="$(basename "$path")"
+# f="./textworks-app.tar.gz"
+# # tar -czf "$f" "$path"
 
-#     # create a zip file if windows release, else create a tar.gz file
-#     fname="$ARTIFACTS_DIR/nRF5-ble-driver_${dirname}_$1_compiled-binaries"
-#     if echo "$dirname" | grep -q "win"; then
-#         f="$fname.zip"
-#         # `-Z store` specifies no compression to be sure zip file is windows compatible: https://superuser.com/questions/5155/how-to-create-a-zip-file-compatible-with-windows-under-linux)
-#         zip -Z store -r "$f" "$path"
-#     else
-#         f="$fname.tar.gz"
-#         tar -czf "$f" "$path"
-#     fi
+# GH_ASSET="$GH_UPLOAD/repos/$GH_OWNER/watr-works/releases/$release_id/assets?name=$(basename "$f")&access_token=$GH_ACCESS_TOKEN"
+# echo "asset = $GH_ASSET"
 
-#     GH_ASSET="https://uploads.github.com/repos/$GH_OWNER/pc-ble-driver/releases/$release_id/assets?name=$(basename "$f")&access_token=$GH_ACCESS_TOKEN"
-#     curl --data-binary @"$f" -H "Content-Type: application/octet-stream" "$GH_ASSET"
-# done
+# curl --data-binary @"$f" -H "Content-Type: application/octet-stream" "$GH_ASSET"
 
 # echo "Release successful!"
 # exit 0
+# ~/p/t/r/watr-works git:acs-dev ❯❯❯ scripts/release-textworks-binary.sh | jq '.'                                                                                                                                                                                                          ✱
+# {
+#   "name": "acs-dev",
+#   "commit": {
+#     "sha": "ef032cf9fc60c418fac736def2bdeadba4664039",
+#     "node_id": "MDY6Q29tbWl0NDY0MjY1MDQ6ZWYwMzJjZjlmYzYwYzQxOGZhYzczNmRlZjJiZGVhZGJhNDY2NDAzOQ==",
+#     "commit": {
+#       "author": {
+#         "name": "Adam Saunders",
+#         "email": "saunders@cs.umass.edu",
+#         "date": "2019-07-08T15:36:49Z"
+#       },
+#       "committer": {
+#         "name": "Adam Saunders",
+#         "email": "saunders@cs.umass.edu",
+#         "date": "2019-07-08T15:36:49Z"
+#       },
+#       "message": "Setting version to 0.12-SNAPSHOT",
+#       "tree": {
+#         "sha": "994b4b35e0836d03c1a180b933da54e67d1f4757",
+#         "url": "https://api.github.com/repos/iesl/watr-works/git/trees/994b4b35e0836d03c1a180b933da54e67d1f4757"
+#       },
+#       "url": "https://api.github.com/repos/iesl/watr-works/git/commits/ef032cf9fc60c418fac736def2bdeadba4664039",
+#       "comment_count": 0,
+#       "verification": {
+#         "verified": false,
+#         "reason": "unsigned",
+#         "signature": null,
+#         "payload": null
+#       }
+#     },
+#     "url": "https://api.github.com/repos/iesl/watr-works/commits/ef032cf9fc60c418fac736def2bdeadba4664039",
+#     "html_url": "https://github.com/iesl/watr-works/commit/ef032cf9fc60c418fac736def2bdeadba4664039",
+#     "comments_url": "https://api.github.com/repos/iesl/watr-works/commits/ef032cf9fc60c418fac736def2bdeadba4664039/comments",
+#     "author": {
+#       "login": "adamchandra",
+#       "id": 411952,
+#       "node_id": "MDQ6VXNlcjQxMTk1Mg==",
+#       "avatar_url": "https://avatars0.githubusercontent.com/u/411952?v=4",
+#       "gravatar_id": "",
+#       "url": "https://api.github.com/users/adamchandra",
+#       "html_url": "https://github.com/adamchandra",
+#       "followers_url": "https://api.github.com/users/adamchandra/followers",
+#       "following_url": "https://api.github.com/users/adamchandra/following{/other_user}",
+#       "gists_url": "https://api.github.com/users/adamchandra/gists{/gist_id}",
+#       "starred_url": "https://api.github.com/users/adamchandra/starred{/owner}{/repo}",
+#       "subscriptions_url": "https://api.github.com/users/adamchandra/subscriptions",
+#       "organizations_url": "https://api.github.com/users/adamchandra/orgs",
+#       "repos_url": "https://api.github.com/users/adamchandra/repos",
+#       "events_url": "https://api.github.com/users/adamchandra/events{/privacy}",
+#       "received_events_url": "https://api.github.com/users/adamchandra/received_events",
+#       "type": "User",
+#       "site_admin": false
+#     },
+#     "committer": {
+#       "login": "adamchandra",
+#       "id": 411952,
+#       "node_id": "MDQ6VXNlcjQxMTk1Mg==",
+#       "avatar_url": "https://avatars0.githubusercontent.com/u/411952?v=4",
+#       "gravatar_id": "",
+#       "url": "https://api.github.com/users/adamchandra",
+#       "html_url": "https://github.com/adamchandra",
+#       "followers_url": "https://api.github.com/users/adamchandra/followers",
+#       "following_url": "https://api.github.com/users/adamchandra/following{/other_user}",
+#       "gists_url": "https://api.github.com/users/adamchandra/gists{/gist_id}",
+#       "starred_url": "https://api.github.com/users/adamchandra/starred{/owner}{/repo}",
+#       "subscriptions_url": "https://api.github.com/users/adamchandra/subscriptions",
+#       "organizations_url": "https://api.github.com/users/adamchandra/orgs",
+#       "repos_url": "https://api.github.com/users/adamchandra/repos",
+#       "events_url": "https://api.github.com/users/adamchandra/events{/privacy}",
+#       "received_events_url": "https://api.github.com/users/adamchandra/received_events",
+#       "type": "User",
+#       "site_admin": false
+#     },
+#     "parents": [
+#       {
+#         "sha": "a9fe6094cebed68f81ef5cb3bc52e364752049f4",
+#         "url": "https://api.github.com/repos/iesl/watr-works/commits/a9fe6094cebed68f81ef5cb3bc52e364752049f4",
+#         "html_url": "https://github.com/iesl/watr-works/commit/a9fe6094cebed68f81ef5cb3bc52e364752049f4"
+#       }
+#     ]
+#   },
+#   "_links": {
+#     "self": "https://api.github.com/repos/iesl/watr-works/branches/acs-dev",
+#     "html": "https://github.com/iesl/watr-works/tree/acs-dev"
+#   },
+#   "protected": false,
+#   "protection": {
+#     "enabled": false,
+#     "required_status_checks": {
+#       "enforcement_level": "off",
+#       "contexts": []
+#     }
+#   },
+#   "protection_url": "https://api.github.com/repos/iesl/watr-works/branches/acs-dev/protection"
+# }
