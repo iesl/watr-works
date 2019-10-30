1d4bcaf7725dbc737c2403ae94944794aa3085b0
Adam Saunders <saunders@cs.umass.edu>
2019-10-30 14:27:23 -0400

    testing release process

--- project/release.scala
+++ project/release.scala
@@ -1,5 +1,7 @@
 import sbt._
-// import sbt.Keys._
+import sbt.Keys._
+
+import scala.sys.process._
 
 import sbtrelease.ReleasePlugin.autoImport._
 import sbtrelease.ReleaseStateTransformations._
@@ -9,23 +11,26 @@ import PKeys._
 
 object Release {
 
+  lazy val textworks = (project in file("text-works"))
 
-  val copyVersionToFile = ReleaseStep(action = st => {
+  val uploadAssetsStep = ReleaseStep(action = st => {
     val extracted = Project.extract(st)
     val version = extracted.get(Keys.version)
     val baseDir = extracted.get(Keys.baseDirectory)
-    val versionFile: File = baseDir / "set-release-versions"
-
-    val ver = s"""|VERSION_STR=v$version
-                  |VERSION_NUM=$version
-                  |""".stripMargin
-
-    IO.write(versionFile, ver)
+    val shell: Seq[String] = if (sys.props("os.name").contains("Windows")) Seq("cmd", "/c") else Seq("bash", "-c")
+    val uploadAssets: Seq[String] = shell :+ s"echo ghr v${version} text-works/target/universal/textworks-${version}.tgz"
+    println(s"uploading assets to release version v${version}")
+    if((uploadAssets !) == 0) {
+      println("upload successful!")
+    } else {
+      throw new IllegalStateException("upload failed!")
+    }
 
     st
   })
 
-  val pkgTgzStep: ReleaseStep = releaseStepTask(Universal / PKeys.packageZipTarball)
+
+  val pkgTgzStep: ReleaseStep = releaseStepTask(textworks / Universal / PKeys.packageZipTarball)
 
   val settings = Seq(
     releaseProcess := Seq[ReleaseStep](
@@ -35,11 +40,45 @@ object Release {
       setReleaseVersion,
       commitReleaseVersion,
       tagRelease,
-      copyVersionToFile,
+      pushChanges,
+      pkgTgzStep,
+      uploadAssetsStep,
       setNextVersion,
       commitNextVersion,
       pushChanges,
   ))
 
+      // copyVersionToFile,
 
 }
+
+  // lazy val uploadAssetsTask = taskKey[Unit]("Upload binary assets to github")
+
+  // uploadAssetsTask := {
+  //   val s: TaskStreams = streams.value
+  //   val shell: Seq[String] = if (sys.props("os.name").contains("Windows")) Seq("cmd", "/c") else Seq("bash", "-c")
+  //   // val npmInstall: Seq[String] = shell :+ "npm install"
+  //   val ver = version.value
+  //   val uploadAssets: Seq[String] = shell :+ s"echo ghr v${ver} text-works/target/universal/textworks-${ver}.tgz"
+  //   s.log.info(s"uploading assets to release version v${ver}")
+  //   if((uploadAssets !) == 0) {
+  //     s.log.success("upload successful!")
+  //   } else {
+  //     throw new IllegalStateException("upload failed!")
+  //   }
+  // }
+
+  // val copyVersionToFile = ReleaseStep(action = st => {
+  //   val extracted = Project.extract(st)
+  //   val version = extracted.get(Keys.version)
+  //   val baseDir = extracted.get(Keys.baseDirectory)
+  //   val versionFile: File = baseDir / "set-release-versions"
+
+  //   val ver = s"""|VERSION_STR=v$version
+  //                 |VERSION_NUM=$version
+  //                 |""".stripMargin
+
+  //   IO.write(versionFile, ver)
+
+  //   st
+  // })
